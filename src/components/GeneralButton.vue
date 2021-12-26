6da7d95934ef54398f6e63535592c7a45ce4b38f
Aleksandrov Sergey <Aleks140988@gmail.com>
2021-12-26 14:13:07 +0200

    first commit

---
+++ src/components/GeneralButton.vue
@@ -0,0 +1,77 @@
+<template>
+  <button
+    class="btn"
+    :style="options"
+    :type="type"
+    :disabled="disabled"
+    @mouseover="changeColor"
+    @mouseleave="originalColor"
+    @click="onHandler"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "GeneralButton",
+  props: {
+    type: String,
+    customBackgroundColor: String,
+    customColor: String,
+    hoverBackgroundColor: String,
+    hoverColor: String,
+    borderRadius: String,
+    border: String,
+    minHeight: String,
+    minWidth: String,
+    disabled: Boolean,
+    fontSize: String,
+    fontWeight: String
+  },
+  data() {
+    return {
+      options: {
+        backgroundColor: this.customBackgroundColor,
+        color: this.customColor,
+        borderRadius: this.borderRadius,
+        minWidth: this.minWidth,
+        minHeight: this.minHeight,
+        fontSize: this.fontSize,
+        fontWeight: this.fontWeight,
+        border: this.border,
+        transition: 'all .3s'
+      }
+    }
+  },
+  methods: {
+    originalColor() {
+      return (
+        this.options.backgroundColor = this.customBackgroundColor,
+        this.options.color = this.customColor
+      )
+    },
+    changeColor() {
+      return (
+        this.options.backgroundColor = this.hoverBackgroundColor,
+        this.options.color = this.hoverColor
+      )
+    },
+    onHandler() {
+      this.$emit('onClick')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .btn {
+    display: inline-grid;
+    justify-content: center;
+    padding: 10px 15px;
+    align-items: center;
+    outline: none;
+    border: none;
+    cursor: pointer;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,40 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GeneralButton
+        type="button"
+        customBackgroundColor="blue"
+        hoverBackgroundColor="#000"
+        customColor="#f00"
+        hoverColor="blue"
+        border="1px solid red"
+        borderRadius="10px"
+        minHeight="50px"
+        minWidth="200px"
+        fontSize="20px"
+        fontWeight="600"
+        @onClick="test"
+    >
+      Сохранить
+    </GeneralButton>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import GeneralButton from '@/components/GeneralButton'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    GeneralButton
+  },
+  methods: {
+    test() {
+      console.log(111)
+    }
   }
 }
 </script>
